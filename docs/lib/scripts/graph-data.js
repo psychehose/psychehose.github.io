082acc8287e7b3e40ce3f42d35cb10f9e661ce15
psychehose <psychehose@gmail.com>
2024-12-30 00:16:40 +0900

    update

---
+++ docs/lib/scripts/graph-data.js
@@ -0,0 +1,1 @@
+let graphData={graphOptions:{attractionForce:1,linkLength:10,repulsionForce:150,centralForce:3,edgePruning:100,minNodeRadius:3,maxNodeRadius:7},isInitialized:!0,paths:["src/index.html","src/test.html"],nodeCount:2,linkSources:[],linkTargets:[],labels:["index","test"],radii:[null,null],linkCount:0}
